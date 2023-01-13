20e4072ef6aad4475bc85932a2730967f0d4d865
ALexSer <5440918@gmail.com>
2023-01-13 22:53:57 +0300

    Task 3 ready

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,10 @@
 ﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+string ChetNechet = "нечётное";
+Console.Write("Введите любое целое число:");
+int num1 = int.Parse(Console.ReadLine());
+
+if (num1%2==0)
+         ChetNechet = "чётное";
+
+Console.WriteLine("Число " + num1 + " - " +ChetNechet + "!");

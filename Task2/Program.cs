10248339c51ebe5cd5350b52d158da47b02d39f6
ALexSer <5440918@gmail.com>
2023-01-13 22:42:26 +0300

    Task 2 ready

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// Возможно применение если чисел не 3, а любое количество и ввод последовательный.
 
+int maxnum = 0;
+int i=0;
+for(i=1; i<=3 ; i++)
+    {
+        Console.Write("Введите " + i + " число: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+        if (i == 1) 
+            maxnum = num1;           
+        else 
+            if (num1 > maxnum)  maxnum = num1;
+    }
+Console.WriteLine("Самое большое число из трёх это: " + maxnum);
